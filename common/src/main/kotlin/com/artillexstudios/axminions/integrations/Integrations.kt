4bf63d2aa93cbfb1887f550ef9c9f5dbfff9a5d5
michuuu-x <michuuu.200@gmail.com>
2026-07-25 20:24:16 +0200

    Remove unused zShop integrations

--- common/src/main/kotlin/com/artillexstudios/axminions/integrations/Integrations.kt
+++ common/src/main/kotlin/com/artillexstudios/axminions/integrations/Integrations.kt
@@ -15,7 +15,6 @@ import com.artillexstudios.axminions.integrations.prices.CMIIntegration
 import com.artillexstudios.axminions.integrations.prices.EconomyShopGUIIntegration
 import com.artillexstudios.axminions.integrations.prices.EssentialsIntegration
 import com.artillexstudios.axminions.integrations.prices.ShopGUIPlusIntegration
-//import com.artillexstudios.axminions.integrations.prices.ZShopIntegration
 import com.artillexstudios.axminions.integrations.protection.BentoBoxIntegration
 import com.artillexstudios.axminions.integrations.protection.GriefPreventionIntegration
 import com.artillexstudios.axminions.integrations.protection.IridiumSkyBlockIntegration
@@ -118,12 +117,6 @@ class Integrations : Integrations {
                         .sendMessage(StringUtils.formatToString("<#33FF33>[AxMinions] Hooked into EconomyShopGUI!"))
                 }
             }
-
-//            "zshop" -> {
-//                if (isPluginLoaded("zShop")) {
-//                    register(ZShopIntegration())
-//                }
-//            }
         }
 
         when (Config.ECONOMY_HOOK().lowercase(Locale.ENGLISH)) {
